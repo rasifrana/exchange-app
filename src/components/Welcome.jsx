3154afcdfb62ed3b9e77206571a7548ed1a4ee80
Asif Rana <muhammadasif@Muhammads-MBP.station>
2022-03-11 21:47:14 +0000

    welcome page changes

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -43,7 +43,7 @@ const Welcome = () => {
             <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
                 <div className="flex  justify-start items-start flex-col mf:mr-10">
                     <h1 className="text-3xl sm:text-5xl text-slate-700 py-1">
-                        Send Crypto <br /> across the world
+                        Sending Crypto <br /> made Easy.
                     </h1>
                     <p className="text-left mt-5  font-light md:w-9/12 w-11/12 text-base">
                         Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
