3a6b6b1522429ce81b128ae40b11d46a3f5551c7
Asif Rana <muhammadasif@Muhammads-MBP.station>
2022-03-11 21:31:42 +0000

    pushing to gh-pages

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,6 @@
 import abi from './Transactions.json';
 
+// "build": "vite build",
 
 export const contractABI = abi.abi;
 export const contractAddress = '0x11F20ce0a06047D203767C60497FE86436Fed0A4';
